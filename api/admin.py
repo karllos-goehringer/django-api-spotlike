764977a0d07a3ca56e2f3808670d22821fd01d7a
Karllos Goehringer <karlloshmg@gmail.com>
2026-04-14 10:25:10 -0300

    showw

--- api/admin.py
+++ api/admin.py
@@ -2,40 +2,40 @@ from django.contrib import admin
 from .models import Artist, Band, Album, GeneroMusical, Songs, Playlist, Users, Albumartist, Albumband, Artistsband, Songsplaylist, Usersplaylists
 
 class UsersAdmin(admin.ModelAdmin):
-    list_display = ('pk_userid', 'email', 'profilepicture')
+    list_display = ('PK_userID', 'email', 'profilepicture')
     pass
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('pk_artistid', 'name', 'description', 'imageartist')
+    list_display = ('PK_artistID', 'name', 'description', 'imageArtist')
     pass
 class BandAdmin(admin.ModelAdmin):
-    list_display = ('pk_bandid', 'name', 'description', 'imageband')
+    list_display = ('PK_bandID', 'name', 'description', 'imageBand')
     pass
 class AlbumAdmin(admin.ModelAdmin):
-    list_display = ('pk_albumid', 'albumname', 'releasedate', 'albumimage')
+    list_display = ('PK_albumID', 'albumName', 'releaseDate', 'albumimage')
     pass
 class GeneroMusicalAdmin(admin.ModelAdmin):
     list_display = ('idgeneroMusical', 'nomeGenero')
     pass
 class SongsAdmin(admin.ModelAdmin):
-    list_display = ('PK_songID', 'songTitle', 'songPath', 'timeMusic', 'lyrics','genero')
+    list_display = ('PK_songID', 'songtitle', 'songpath', 'timeMusic', 'lyrics', 'generomusical_idgeneroMusical')
     pass
 class PlaylistAdmin(admin.ModelAdmin):
-    list_display = ('pk_playlistid', 'plname')
+    list_display = ('PK_playlistID', 'plName')
     pass
 class AlbumartistAdmin(admin.ModelAdmin):
-    list_display = ('album', 'artist')
+    list_display = ('album_PK_albumID', 'artist_PK_artistID')
     pass
 class AlbumbandAdmin(admin.ModelAdmin):
-    list_display = ('album', 'band')
+    list_display = ('album_PK_albumID', 'band_PK_bandID')
     pass
 class ArtistsbandAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'band')
+    list_display = ('artist_PK_artistID', 'band_PK_bandID')
     pass
 class SongsplaylistAdmin(admin.ModelAdmin):
-    list_display = ('songs', 'playlist')
+    list_display = ('playlist_PK_playlistID', 'songs_PK_songID', 'ordem')
     pass
-class UsersplaylistAdmin(admin.ModelAdmin):
-    list_display = ('users', 'playlist')
+class UsersplaylistsAdmin(admin.ModelAdmin):
+    list_display = ('users_PK_userID', 'playlist_PK_playlistID')
     pass
 
 admin.site.register(Users, UsersAdmin)
@@ -49,4 +49,4 @@ admin.site.register(Albumartist, AlbumartistAdmin)
 admin.site.register(Albumband, AlbumbandAdmin)
 admin.site.register(Artistsband, ArtistsbandAdmin)
 admin.site.register(Songsplaylist, SongsplaylistAdmin)
-admin.site.register(Usersplaylists, UsersplaylistAdmin)
+admin.site.register(Usersplaylists, UsersplaylistsAdmin)
--- api/models.py
+++ api/models.py
@@ -1,125 +1,127 @@
 ﻿from django.db import models
 
 class Artist(models.Model):
-    pk_artistid = models.AutoField(db_column='PK_artistID', primary_key=True)
+    PK_artistID = models.AutoField(db_column='PK_artistID', primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    imageartist = models.FileField(upload_to='images/')
+    imageArtist = models.ImageField(upload_to='images/', blank=True, null=True, db_column='imageArtist')
 
     class Meta:
         managed = False
         db_table = 'artist'
 
 class Band(models.Model):
-    pk_bandid = models.AutoField(db_column='PK_bandID', primary_key=True)
+    PK_bandID = models.AutoField(db_column='PK_bandID', primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300, blank=True, null=True)
-    imageband = models.FileField(upload_to='images/')
+    imageBand = models.ImageField(upload_to='images/', blank=True, null=True, db_column='imageBand')
 
     class Meta:
         managed = False
         db_table = 'band'
 
 class Album(models.Model):
-    pk_albumid = models.AutoField(db_column='PK_albumID', primary_key=True)
-    albumname = models.CharField(db_column='albumName', max_length=100)
-    releasedate = models.DateField(db_column='releaseDate', blank=True, null=True)
-    albumimage =  models.FileField(upload_to='images/')
+    PK_albumID = models.AutoField(db_column='PK_albumID', primary_key=True)
+    albumName = models.CharField(db_column='albumName', max_length=100)
+    releaseDate = models.DateField(db_column='releaseDate', blank=True, null=True)
+    albumimage = models.ImageField(upload_to='images/', blank=True, null=True, db_column='albumimage')
+
     class Meta:
         managed = False
         db_table = 'album'
 
-from django.db import models
-
 class GeneroMusical(models.Model):
-    idgeneroMusical = models.AutoField(primary_key=True)
-    nomeGenero = models.CharField(max_length=45)
+    idgeneroMusical = models.AutoField(primary_key=True, db_column='idgeneroMusical')
+    nomeGenero = models.CharField(max_length=150, db_column='nomeGenero')
 
     class Meta:
+        managed = False
         db_table = 'generomusical'
 
 class Songs(models.Model):
-    PK_songID = models.AutoField(primary_key=True)
-    songTitle = models.CharField(max_length=150)
-    songPath = models.FileField(upload_to='songs/')
-    timeMusic = models.TimeField(null=True, blank=True)
-    lyrics = models.TextField(null=True, blank=True)
-    genero = models.ForeignKey(
-        GeneroMusical, 
-        on_delete=models.CASCADE, 
+    PK_songID = models.AutoField(db_column='PK_songID', primary_key=True)
+    songtitle = models.CharField(max_length=150, db_column='songTitle')
+    timeMusic = models.TimeField(blank=True, null=True, db_column='timeMusic')
+    lyrics = models.TextField(blank=True, null=True)
+    songpath = models.FileField(upload_to='songs/', blank=True, null=True, db_column='songpath')
+    generomusical_idgeneroMusical = models.ForeignKey(
+        GeneroMusical,
+        on_delete=models.CASCADE,
         db_column='generomusical_idgeneroMusical'
     )
-    album = models.ForeignKey(
+    album_pk_albumid1 = models.ForeignKey(
         Album,
         on_delete=models.CASCADE,
-        db_column='album_PK_albumID'
+        db_column='album_PK_albumID1'
     )
 
     class Meta:
+        managed = False
         db_table = 'songs'
+
 class Playlist(models.Model):
-    pk_playlistid = models.AutoField(db_column='PK_playlistID', primary_key=True)
-    plname = models.CharField(db_column='plName', max_length=45, blank=True, null=True)
+    PK_playlistID = models.AutoField(db_column='PK_playlistID', primary_key=True)
+    plName = models.CharField(db_column='plName', max_length=45, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'playlist'
 
 class Users(models.Model):
-    pk_userid = models.AutoField(db_column='PK_userID', primary_key=True)
+    PK_userID = models.AutoField(db_column='PK_userID', primary_key=True)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=250, blank=True, null=True)
     senha = models.CharField(max_length=45)
-    profilepicture = models.CharField(db_column='profilePicture', max_length=2048, blank=True, null=True)
+    profilepicture = models.ImageField(upload_to='images/', blank=True, null=True, db_column='profilePicture')
 
     class Meta:
         managed = False
         db_table = 'users'
 
-# TABELAS INTERMEDIÁRIAS 
+# TABELAS INTERMEDIÁRIAS
 
 class Albumartist(models.Model):
-    artist = models.ForeignKey(Artist, models.DO_NOTHING, db_column='artist_PK_artistID', primary_key=True)
-    album = models.ForeignKey(Album, models.DO_NOTHING, db_column='album_PK_albumID')
+    artist_PK_artistID = models.ForeignKey(Artist, on_delete=models.CASCADE, db_column='artist_PK_artistID', primary_key=True)
+    album_PK_albumID = models.ForeignKey(Album, on_delete=models.CASCADE, db_column='album_PK_albumID')
 
     class Meta:
         managed = False
         db_table = 'albumartist'
-        unique_together = (('artist', 'album'),)
+        unique_together = (('artist_PK_artistID', 'album_PK_albumID'),)
 
 class Albumband(models.Model):
-    band = models.ForeignKey(Band, models.DO_NOTHING, db_column='band_PK_bandID', primary_key=True)
-    album = models.ForeignKey(Album, models.DO_NOTHING, db_column='album_PK_albumID')
+    band_PK_bandID = models.ForeignKey(Band, on_delete=models.CASCADE, db_column='band_PK_bandID', primary_key=True)
+    album_PK_albumID = models.ForeignKey(Album, on_delete=models.CASCADE, db_column='album_PK_albumID')
 
     class Meta:
         managed = False
         db_table = 'albumband'
-        unique_together = (('band', 'album'),)
-
+        unique_together = (('band_PK_bandID', 'album_PK_albumID'),)
 
 class Artistsband(models.Model):
-    artist = models.ForeignKey(Artist, models.DO_NOTHING, db_column='artist_PK_artistID', primary_key=True)
-    band = models.ForeignKey(Band, models.DO_NOTHING, db_column='band_PK_bandID')
+    artist_PK_artistID = models.ForeignKey(Artist, on_delete=models.CASCADE, db_column='artist_PK_artistID', primary_key=True)
+    band_PK_bandID = models.ForeignKey(Band, on_delete=models.CASCADE, db_column='band_PK_bandID')
 
     class Meta:
         managed = False
         db_table = 'artistsband'
-        unique_together = (('artist', 'band'),)
+        unique_together = (('artist_PK_artistID', 'band_PK_bandID'),)
 
 class Songsplaylist(models.Model):
-    playlist = models.ForeignKey(Playlist, models.DO_NOTHING, db_column='playlist_PK_playlistID', primary_key=True)
-    songs = models.ForeignKey(Songs, models.DO_NOTHING, db_column='songs_PK_songID')
+    PK_songsplaylistID = models.AutoField(primary_key=True, db_column='PK_songsplaylistID')
+    playlist_PK_playlistID = models.ForeignKey(Playlist, on_delete=models.CASCADE, db_column='playlist_PK_playlistID')
+    songs_PK_songID = models.ForeignKey(Songs, on_delete=models.CASCADE, db_column='songs_PK_songID')
+    ordem = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'songsplaylist'
-        unique_together = (('playlist', 'songs'),)
 
 class Usersplaylists(models.Model):
-    users = models.ForeignKey(Users, models.DO_NOTHING, db_column='users_PK_userID', primary_key=True)
-    playlist = models.ForeignKey(Playlist, models.DO_NOTHING, db_column='playlist_PK_playlistID')
+    users_PK_userID = models.ForeignKey(Users, on_delete=models.CASCADE, db_column='users_PK_userID', primary_key=True)
+    playlist_PK_playlistID = models.ForeignKey(Playlist, on_delete=models.CASCADE, db_column='playlist_PK_playlistID')
 
     class Meta:
         managed = False
         db_table = 'usersplaylists'
-        unique_together = (('users', 'playlist'),)
+        unique_together = (('users_PK_userID', 'playlist_PK_playlistID'),)
